a779c342d76824df44b3c9d44dc42aebad8c8c4f
Nehal70 <63651397+Nehal70@users.noreply.github.com>
2025-02-21 23:34:31 -0500

    User Routing Setup

---
+++ server/controllers/userController.js
@@ -0,0 +1,9 @@
+import User from '../models/user.js';
+
+//Create or Find a User
+
+//View User Profile (Read)
+
+//Update User Profile (Usually, height/weight, etc.)
+
+//Delete a User (Delete Account)
