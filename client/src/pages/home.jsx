435a032fd47118f3dae4da6cbf0eab7e351244c7
Nehal70 <63651397+Nehal70@users.noreply.github.com>
2025-02-23 00:02:47 -0500

    random home design

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,31 +1,33 @@
-import Pattern from '../assets/pattern.webp';
+import Pattern from '../assets/logobgstraightened.png';
 import Wordmark from '../assets/wordmark.svg';
 
 function Home() {
 
     return (
         <>
-            <div className='fixed top-0 z-[50] w-screen h-screen  bg-[auto_600px] bg-white/75 bg-blend-lighten'
+            <div className='fixed top-0 z-[50] w-screen h-screen bg-[auto_600px] bg-white/90 bg-blend-lighten font-lato' 
                 style={{ backgroundImage: `url(${Pattern})` }}>
                 <div className='flex justify-between px-10 pt-8 pb-3'>
                     <a href='/'><img className='max-w-[230px]' src={Wordmark} /></a>
                     <div className='grid grid-cols-2 gap-4 justify-center align-middle'>
-                        <a href='signup'><button className='border border-[#1E1E1E] text-[#1E1E1E] my-1 px-8 py-2 rounded-[100px] bg-white hover:bg-fit-white-hover hover:text-black'>Sign Up</button></a>
-                        <a href='login'><button className='bg-fit-orange my-1 px-8 py-2 rounded-[100px] text-white hover:bg-fit-orange-hover hover:text-fit-white-hover'>Log In</button></a>
+                        <a href='signup'><button className='border border-fit-orange text-fit-orange my-1 px-6 py-2 rounded-full bg-white hover:bg-fit-orange hover:text-white font-lato text-sm'>Sign Up</button></a>
+                        <a href='login'><button className='bg-fit-orange my-1 px-6 py-2 rounded-full text-white border border-fit-orange hover:bg-white hover:text-fit-orange font-lato text-sm'>Log In</button></a>
                     </div>
                 </div>
                 <div
-                    className="flex flex-col gap-12 h-[80vh] items-center justify-center px-[30px]">
-                    <h1 className='text-fit-black text-center font-bold text-4xl md:text-5xl lg:text-6xl'>Smarter, safer workouts. <br /> Personalized for you.</h1>
-                    <h4 className='text-fit-gray text-center text-xl w-2/3 max-w-[780px]'>Our AI-powered platform creates workout programs based on your individual needs. Our tailored routines are crafted for injury recovery and making you a fitter, healthier person.</h4>
+                    className="flex flex-col gap-12 h-[80vh] items-center justify-center px-[30px] font-lato">
+                    <h1 className='text-[#333333] text-center font-bold text-4xl md:text-5xl lg:text-6xl animate-floatIn'>Smarter, safer workouts. <br /> Personalized for you.</h1>
+                    <h4 className='text-[#555555] text-center text-sm uppercase opacity-90 w-2/3 max-w-[780px] animate-floatInDelay'>OUR AI-POWERED PLATFORM CREATES WORKOUT PROGRAMS BASED ON YOUR INDIVIDUAL NEEDS. OUR TAILORED ROUTINES ARE CRAFTED FOR INJURY RECOVERY AND MAKING YOU A FITTER, HEALTHIER PERSON.</h4>
 
-                    <a href='signup'><button className='text-2xl font-bold px-13 py-4 bg-fit-orange text-white rounded-[100px] hover:bg-fit-orange-hover hover:text-fit-white-hover'>Get Started</button></a>
+                    <a href='signup'><button className='text-lg font-bold px-6 py-2 bg-fit-orange text-white rounded-full border border-fit-orange hover:bg-white hover:text-fit-orange font-lato'>Get Started</button></a>
                 </div>
             </div>
-
         </>
-
     )
 }
 
-export default Home
+export default Home;
+
+
+
+
